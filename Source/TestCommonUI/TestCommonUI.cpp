7dd4128265182853721e3fe5605ec4981b736ce0
Alexander <alex_c007@hotmail.com>
2024-05-30 01:05:04 +0200

    Initial commit with plain main menu example

---
+++ Source/TestCommonUI/TestCommonUI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestCommonUI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestCommonUI, "TestCommonUI" );
